d2331f9c716275969dfa9bcf787ade427a84138e
Kosyrev143 <artemkosyrev93@gmail.com>
2025-11-02 15:55:02 +0500

    feat: (user) crud

--- src/user/application/use-cases/create-user.use-case.ts
+++ src/user/application/use-cases/create-user.use-case.ts
@@ -1,5 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { User } from '../../domain/entities/user.entity';
+import {
+  USER_REPOSITORY,
+  UserRepositoryPort,
+} from '../ports/user.repository.port';
 
 export interface CreateUserDto {
   name: string;
@@ -8,5 +12,21 @@ export interface CreateUserDto {
 
 @Injectable()
 export class CreateUserUseCase {
-  constructor() {}
+  constructor(
+    @Inject(USER_REPOSITORY)
+    private readonly userRepository: UserRepositoryPort,
+  ) {}
+
+  async execute(dto: CreateUserDto): Promise<User> {
+    const existingUser = await this.userRepository.findByEmail(dto.email);
+
+    if (existingUser) {
+      throw new BadRequestException('User with this email already exists');
+    }
+
+    const user = User.create(dto.name, dto.email);
+    const savedUser = await this.userRepository.save(user);
+
+    return savedUser;
+  }
 }
---
+++ src/user/user.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+
+import { CreateUserUseCase } from './application/use-cases/create-user.use-case';
+import { USER_REPOSITORY } from './application/ports/user.repository.port';
+import { InMemoryUserRepository } from './infrastructure/adapters/in-memory-user.repository';
+import { UserController } from './presentation/user.controller';
+import { GetUserUseCase } from './application/use-cases/get-user.use-case';
+import { ListUsersUseCase } from './application/use-cases/list-users.use-case';
+import { DeleteUserUseCase } from './application/use-cases/delete-user.use-case';
+import { UpdateUserUseCase } from './application/use-cases/update-user.use-case';
+
+@Module({
+  controllers: [UserController],
+  providers: [
+    CreateUserUseCase,
+    GetUserUseCase,
+    ListUsersUseCase,
+    DeleteUserUseCase,
+    UpdateUserUseCase,
+    {
+      provide: USER_REPOSITORY,
+      useClass: InMemoryUserRepository,
+    },
+  ],
+})
+export class UserModule {}
